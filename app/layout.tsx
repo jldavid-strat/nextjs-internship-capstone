b1f7f716bb70d5ae32c6ccce0c1cb0bc88ce22f8
jldavid-strat <i_jed.david@stratpoint.com>
2025-07-23 10:32:25 +0800

    chore: change printWidth prettier rule to 90 and removed singleAttributePerLine

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,11 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
-      <html lang="en" suppressHydrationWarning>
+      {/* TODO: add universal loading on top of the app */}
+      <html
+        lang="en"
+        suppressHydrationWarning
+      >
         <body className={inter.className}>
           <ThemeProvider>{children}</ThemeProvider>
         </body>
