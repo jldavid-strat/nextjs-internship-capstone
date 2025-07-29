7b5b3c349c02202ad82e5d75063b84279f3cf542
jldavid-strat <i_jed.david@stratpoint.com>
2025-07-29 08:35:01 +0800

    feat: add types for clerkUser in user server actions

--- lib/actions/user_actions.ts
+++ lib/actions/user_actions.ts
@@ -1,49 +1,59 @@
 // TODO: create type inference for clerUser
 
-// "use server"
-//  import { db } from '@/lib/db/connect_db';
-// import { user } from '@/lib/db/schema';
-// import { eq } from 'drizzle-orm';
-// import { revalidatePath } from 'next/cache';
-// import { redirect } from 'next/navigation';
+'use server';
+import { db } from '@/lib/db/connect_db';
+import { user } from '@/lib/db/schema';
+import { eq } from 'drizzle-orm';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 
-// export async function createUser({ newUser }) {
-//   try {
-//     await db.insert(user).values({
-//       clerkId: newUser.id,
-//       firstName: newUser.firstName,
-//       lastName: newUser.lastName,
-//       primaryEmailAddress: newUser.primaryEmailAddress,
-//       createdAt: newUser.createdAt,
-//     });
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
+interface clerkUserProps {
+  clerkId: string;
+  firstName: string;
+  lastName: string;
+  primaryEmailAddress: string;
+  createdAt: Date;
+  updatedAt: Date | undefined;
+}
 
-// export async function updatedUser({ clerkUser }) {
-//   try {
-//     await db
-//       .update(user)
-//       .set({
-//         firstName: clerkUser.firstName,
-//         lastName: clerkUser.lastName,
-//         primaryEmailAddress: clerkUser.primaryEmailAddress,
-//         updatedAt: clerkUser.createdAt,
-//       })
-//       .where(eq(user.clerkId, clerkUser.id));
-//     // invalidate cache on settings page to load new user info
-//     revalidatePath(`/settings`);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-//
-// export async function deleteUser({ clerkUser }) {
-//   try {
-//     await db.delete(user).where(eq(user.id, clerkUser.id));
-//     redirect('/sign-up');
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
+export async function createUser(clerkUser: clerkUserProps) {
+  try {
+    await db.insert(user).values({
+      clerkId: clerkUser.clerkId,
+      firstName: clerkUser.firstName,
+      lastName: clerkUser.lastName,
+      primaryEmailAddress: clerkUser.primaryEmailAddress,
+      createdAt: clerkUser.createdAt,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function updatedUser(clerkUser: clerkUserProps) {
+  try {
+    await db
+      .update(user)
+      .set({
+        firstName: clerkUser.firstName,
+        lastName: clerkUser.lastName,
+        primaryEmailAddress: clerkUser.primaryEmailAddress,
+        updatedAt: clerkUser.createdAt,
+      })
+      .where(eq(user.clerkId, clerkUser.clerkId));
+
+    // invalidate cache on settings page to load new user info
+    revalidatePath(`/settings`);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function deleteUser(clerkUser: clerkUserProps) {
+  try {
+    await db.delete(user).where(eq(user.clerkId, clerkUser.clerkId));
+    redirect('/sign-up');
+  } catch (error) {
+    console.error(error);
+  }
+}
