ea442eb04ee2b6b23e879d0f4811f40d873b4333
jldavid-strat <i_jed.david@stratpoint.com>
2025-08-04 14:49:28 +0800

    backend(connect-db): add relations in drizzle relations

--- lib/db/connect_db.ts
+++ lib/db/connect_db.ts
@@ -1,10 +1,11 @@
 import { drizzle } from 'drizzle-orm/neon-http';
 import { neon } from '@neondatabase/serverless';
 import { config } from 'dotenv';
-import * as schema from './schema';
+import * as schema from './schema/schema';
+import * as relations from './relations';
 
 config({ path: '.env' });
 
 const sql = neon(process.env.DATABASE_URL!);
 const client = sql;
-export const db = drizzle(client, { schema });
+export const db = drizzle(client, { schema: { ...schema, ...relations } });
