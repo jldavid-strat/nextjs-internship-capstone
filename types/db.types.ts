fcdba077d99a9d4544be8460c52160a4b3d1c41f
jldavid-strat <i_jed.david@stratpoint.com>
2025-08-20 15:44:31 +0800

    types(db-types): removed team type

--- types/db.types.ts
+++ types/db.types.ts
@@ -1,6 +1,6 @@
 /*  == INFERRED TYPES FROM DB SCHEMA == */
 
-import { projects, tasks, taskComments, teams, users } from '@/lib/db/schema';
+import { projects, tasks, taskComments, users } from '@/lib/db/schema/schema';
 
 export type Project = typeof projects.$inferSelect;
 export type CreateProject = typeof projects.$inferInsert;
@@ -9,10 +9,6 @@ export type UpdateProject = Partial<typeof projects.$inferInsert>;
 export type TaskComment = typeof taskComments.$inferSelect;
 export type CreateTaskComment = typeof taskComments.$inferInsert;
 export type UpdateTaskComment = Partial<typeof taskComments.$inferInsert>;
-
-export type Team = typeof teams.$inferSelect;
-export type CreateTeam = typeof teams.$inferInsert;
-export type UpdateTeam = Partial<typeof teams.$inferInsert>;
 
 export type Task = typeof tasks.$inferSelect;
 export type CreateTask = typeof tasks.$inferInsert;
