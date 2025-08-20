a6eb9d39cde4e8da87aba0d53c248ca7536e6b52
jldavid-strat <i_jed.david@stratpoint.com>
2025-08-20 16:34:05 +0800

    backend(user-queries): added user query by email

--- lib/queries/user.queries.ts
+++ lib/queries/user.queries.ts
@@ -1,12 +1,10 @@
-// TODO: create type inference for clerUser
-
 'use server';
 import { db } from '@/lib/db/connect_db';
 import { users } from '@/lib/db/schema/schema';
 import { QueryResult } from '@/types';
 import { User, CreateUser, UpdateUser } from '@/types/db.types';
 import { auth } from '@clerk/nextjs/server';
-import { eq } from 'drizzle-orm';
+import { and, eq, ilike, notInArray } from 'drizzle-orm';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
@@ -104,5 +102,43 @@ export async function getUserByClerkId(userClerkId: User['clerkId']): Promise<Qu
       message: 'Failed to retrieve user',
       error: JSON.stringify(error),
     };
+  }
+}
+
+// TODO change to server component
+
+export async function getSuggestedUsersByEmail(
+  input: string | null,
+  filterIds: string[] = [],
+): Promise<User[] | []> {
+  try {
+    console.log(filterIds);
+
+    // get suggested users by chunks of eight
+    const suggestedUsers = await db
+      .select()
+      .from(users)
+      .where(
+        and(
+          input ? ilike(users.primaryEmailAddress, `%${input.trim()}%`) : undefined,
+          filterIds.length > 0 ? notInArray(users.id, filterIds) : undefined,
+        ),
+      )
+      .limit(8);
+
+    return suggestedUsers;
+    // return {
+    //   success: true,
+    //   message: 'Successfully retrieve all users',
+    //   data: suggestedUsers,
+    // };
+  } catch (error) {
+    console.error(error);
+    // return {
+    //   success: true,
+    //   message: 'Failed to retrieved users',
+    //   error: JSON.stringify(error),
+    // };
+    return [];
   }
 }
