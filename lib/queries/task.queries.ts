782ad0237a5b220380ba7c37774548843f24a487
jldavid-strat <i_jed.david@stratpoint.com>
2025-08-26 22:23:22 +0800

    backend(task-query): update task list and get task query, implemente getMaxNumPosition query

--- lib/queries/task.queries.ts
+++ lib/queries/task.queries.ts
@@ -2,45 +2,43 @@ import 'server-only';
 import { db } from '../db/connect_db';
 import { tasks } from '../db/schema/schema';
 import { and, eq, max } from 'drizzle-orm';
-import { Project, Task } from '@/types/db.types';
+import { KanbanColumn, Project, Task } from '@/types/db.types';
 
-export async function getMaxPositionByColumnId(
+export async function getMaxNumPositionByColumnId(
   kanbanColumnId: Task['kanbanColumnId'],
   projectId: Task['projectId'],
 ) {
   try {
     const [result] = await db
       .select({
-        maxPosition: max(tasks.position),
+        maxNumPosition: max(tasks.position),
       })
       .from(tasks)
       .where(and(eq(tasks.projectId, projectId), eq(tasks.kanbanColumnId, kanbanColumnId)));
 
-    return result.maxPosition;
+    return result.maxNumPosition;
   } catch (error) {
     console.error(error);
   }
 }
 
 // return type : Promise<QueryResult<Partial<Task[]>>>
-export async function getTaskListByStatus(projectId: Project['id'], status: string) {
+export async function getTaskList(projectId: Project['id'], withColumnId?: KanbanColumn['id']) {
   try {
     const taskList = await db
-      .select({
-        id: tasks.id,
-        title: tasks.title,
-        description: tasks.description,
-        priority: tasks.priority,
-        detail: tasks.detail,
-        startDate: tasks.startDate,
-        dueDate: tasks.dueDate,
-      })
+      .select()
       .from(tasks)
-      .where(and(eq(tasks.projectId, projectId), eq(tasks.status, status)))
+      .where(
+        and(
+          eq(tasks.projectId, projectId),
+          withColumnId ? eq(tasks.kanbanColumnId, withColumnId) : undefined,
+        ),
+      )
       .orderBy(tasks.position);
 
     return { success: true, message: `Successfully fetched task list`, data: taskList };
   } catch (error) {
+    console.error(error);
     return {
       success: false,
       message: `Failed to fetch task list`,
@@ -49,13 +47,13 @@ export async function getTaskListByStatus(projectId: Project['id'], status: stri
   }
 }
 
-export async function getTaskById(taskId: Task['id'], projectId: Project['id']) {
+export async function getTaskById(taskId: Task['id']) {
   try {
     const task = await db.query.tasks.findFirst({
-      where: (tasks, { and, eq }) => and(eq(tasks.id, taskId), eq(tasks.projectId, projectId)),
+      where: (tasks, { and, eq }) => and(eq(tasks.id, taskId)),
     });
 
-    return { success: true, message: `Successfully fetched task list`, data: task };
+    return { success: true, message: `Successfully fetched task `, data: task };
   } catch (error) {
     return {
       success: false,
