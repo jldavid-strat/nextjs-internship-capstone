72150c5ecdfcc595e502ab0c50f884a930cd4d2b
jldavid-strat <i_jed.david@stratpoint.com>
2025-08-28 15:09:46 +0800

    modify(layout): update html inherit theme attribute

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
-import { ThemeProvider } from '@/components/theme-provider';
+import { ThemeProvider } from '@/providers/theme-provider';
 import Providers from '@/providers/query-provider';
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,11 +17,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <ClerkProvider>
       {/* TODO: add universal loading on top of the app */}
-      <html lang="en">
-        <body className={inter.className}>
-          <Providers>
-            <ThemeProvider>{children}</ThemeProvider>
-          </Providers>
+      <html lang="en" suppressHydrationWarning>
+        <body className={`${inter.className}`}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Providers>{children}</Providers>
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
