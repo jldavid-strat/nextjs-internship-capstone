5dc78f1936d54b4cec9e5b32d1527c43024c1905
jldavid-strat <i_jed.david@stratpoint.com>
2025-08-29 16:28:12 +0800

    constants(project-labels): added default project label values

--- constants/labels.ts
+++ constants/labels.ts
@@ -1,15 +1,31 @@
-export const DEFAULT_LABEL_COLORS = [
-  '#10B981',
-  '#3B82F6',
-  '#EF4444',
-  '#8B5CF6',
-  '#F59E0B',
+type DefaultLabel = {
+  labelName: string;
+  color: string;
+};
+
+export const DEFAULT_LABELS: DefaultLabel[] = [
+  {
+    labelName: 'bug',
+    color: '#EF4444', //red
+  },
+  {
+    labelName: 'refactor',
+    color: '#3B82F6', //blue
+  },
+  {
+    labelName: 'feature',
+    color: '#10B981', // green
+  },
+  {
+    labelName: 'production',
+    color: '#8B5CF6', // purple
+  },
+  {
+    labelName: 'documentation',
+    color: '#F59E0B', // amber
+  },
 ] as const;
 
-export const DEFAULT_LABELS = [
-  'bug',
-  'feature',
-  'documentation',
-  'production',
-  'refactor',
-] as const;
+export const defaultLabelNames: Array<DefaultLabel['labelName']> = DEFAULT_LABELS.map(
+  (l) => l.labelName,
+);
