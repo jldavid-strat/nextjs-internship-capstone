93bc83d48e4ca1f7e23d95000db8d796f35c2348
jldavid-strat <i_jed.david@stratpoint.com>
2025-08-29 14:27:19 +0800

    chore: added constant file label related default values

---
+++ constants/labels.ts
@@ -0,0 +1,15 @@
+export const DEFAULT_LABEL_COLORS = [
+  '#10B981',
+  '#3B82F6',
+  '#EF4444',
+  '#8B5CF6',
+  '#F59E0B',
+] as const;
+
+export const DEFAULT_LABELS = [
+  'bug',
+  'feature',
+  'documentation',
+  'production',
+  'refactor',
+] as const;
