911cc8835e3bf5409443f88a0aef93d2ecac4799
jldavid-strat <i_jed.david@stratpoint.com>
2025-08-29 11:14:38 +0800

    refactor(modal): remove state variables and added way to add modal open trigger component

--- components/ui/modal.tsx
+++ components/ui/modal.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
-import { Dialog, DialogOverlay, DialogContent } from './dialog';
+import { Dialog, DialogContent } from './dialog';
+import { DialogTitle, DialogTrigger } from '@radix-ui/react-dialog';
+import { cn } from '../../lib/utils/shadcn-utils';
 
 export type ModalProps = {
-  isOpen: boolean;
-  setIsOpen: (input: boolean) => void;
+  triggerComponent: React.ReactNode;
   children: React.ReactNode;
+  className?: string;
 };
 
-export default function Modal({ isOpen, setIsOpen, children }: ModalProps) {
+export default function Modal({ children, className, triggerComponent }: ModalProps) {
   return (
-    <Dialog open={isOpen} onOpenChange={setIsOpen}>
-      <DialogOverlay>
-        <DialogContent className="border-border overflow-y-hidden">{children}</DialogContent>
-      </DialogOverlay>
+    <Dialog>
+      <DialogTrigger asChild>{triggerComponent}</DialogTrigger>
+      <DialogContent
+        className={cn('border-border max-h-screen overflow-y-scroll lg:max-w-screen-lg', className)}
+      >
+        <DialogTitle className="sr-only"></DialogTitle>
+
+        {/* 
+        
+        Add <DialogClose aSchild> in children to add a trigger to manually close the modal
+        
+        */}
+        {children}
+      </DialogContent>
     </Dialog>
   );
 }
