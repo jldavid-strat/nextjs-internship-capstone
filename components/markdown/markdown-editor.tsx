829c4bd7aca7ba4d5edfe4e5c8d70c8c961926c5
jldavid-strat <i_jed.david@stratpoint.com>
2025-08-29 16:06:23 +0800

    modify(md-editor): removed not needed custom button

--- components/markdown/markdown-editor.tsx
+++ components/markdown/markdown-editor.tsx
@@ -12,7 +12,7 @@ type MardownEditorProps = {
   onValueChange: (value: string) => void;
 };
 
-const Button = () => {
+const CodePreview = () => {
   const { preview, dispatch } = useContext(EditorContext);
   const click = () => {
     dispatch!({
@@ -26,28 +26,11 @@ const codePreview: ICommand = {
   name: 'preview',
   keyCommand: 'preview',
   value: 'preview',
-  icon: <Button />,
-};
-const Disable = () => {
-  const { preview } = useContext(EditorContext);
-  return (
-    <button disabled={preview === 'preview'}>
-      <svg viewBox="0 0 16 16" width="12px" height="12px">
-        <path
-          d="M8 0C3.6 0 0 3.6 0 8s3.6 8 8 8 8-3.6 8-8-3.6-8-8-8Zm.9 13H7v-1.8h1.9V13Zm-.1-3.6v.5H7.1v-.6c.2-2.1 2-1.9 1.9-3.2.1-.7-.3-1.1-1-1.1-.8 0-1.2.7-1.2 1.6H5c0-1.7 1.2-3 2.9-3 2.3 0 3 1.4 3 2.3.1 2.3-1.9 2-2.1 3.5Z"
-          fill="currentColor"
-        />
-      </svg>
-    </button>
-  );
+  icon: <CodePreview />,
 };
 
-const customButton = {
-  name: 'disable',
-  keyCommand: 'disable',
-  value: 'disable',
-  icon: <Disable />,
-};
+// [CONSIDER]
+// https://www.shadcn.io/components/forms/minimal-tiptap
 
 export default function MarkdownEditor({ value, onValueChange }: MardownEditorProps) {
   return (
