642e933af7cdd28c0b5d168d946730c4c9258191
jldavid-strat <i_jed.david@stratpoint.com>
2025-08-29 11:16:31 +0800

    refactor(create-task-moda): wrapped create-task form in modal component

--- components/modals/create-task-modal.tsx
+++ components/modals/create-task-modal.tsx
@@ -1,14 +1,20 @@
 'use client';
 
 import { createTask } from '@/actions/task.actions';
-import { X } from 'lucide-react';
-import { startTransition, useActionState, useLayoutEffect, useRef } from 'react';
-import { useForm } from 'react-hook-form';
-import { TaskSchema } from '@/lib/validations';
+import { startTransition, useActionState, useLayoutEffect, useRef, useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import { TaskSchema } from '@/lib/validations/task.validations';
 import z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useRouter } from 'next/navigation';
 import { TASK_PRIORITY_VALUES } from '@/lib/db/schema/enums';
+import { Input } from '../ui/input';
+import MarkdownEditor from '../markdown/markdown-editor';
+import Modal from '../ui/modal';
+import { Button } from '../ui/button';
+import { capitalize } from 'lodash';
+import { AddMemberMultiSelect } from '../ui/add-member-multiselect';
+import { DialogClose } from '../ui/dialog';
 // TODO: Task 4.4 - Build task creation and editing functionality
 // TODO: Task 5.6 - Create task detail modals and editing interfaces
 
@@ -71,21 +77,18 @@ type CreateTaskProps = {
   kanbanName: string;
 };
 
-export function CreateTaskModal({
-  kanbanData,
-  setIsOpen,
-}: {
-  kanbanData: CreateTaskProps;
-  setIsOpen: (setValue: boolean) => void;
-}) {
+export function CreateTaskModal({ kanbanData }: { kanbanData: CreateTaskProps }) {
   const [state, createTaskAction, isPending] = useActionState(
     createTask.bind(null, kanbanData),
     undefined,
   );
 
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(true);
+
   const {
     register,
     handleSubmit,
+    control,
     formState: { errors },
   } = useForm<FormTaskType>({
     resolver: zodResolver(FormTaskSchema),
@@ -110,155 +113,167 @@ export function CreateTaskModal({
   // NOTE only handle success state
   useLayoutEffect(() => {
     if (state?.success) {
-      setIsOpen(false);
+      // setIsOpen(false);
       alert('Successfully created task');
 
       // redirect to newly project page
       // router.push(`/projects/${state.data}`);
     }
-  }, [state, router, setIsOpen]);
+  }, [state, router]);
 
   return (
-    <div className="bg-opacity-50 fixed inset-0 z-50 flex items-center justify-center bg-black/70">
-      <div className="dark:bg-outer_space-500 mx-4 w-full max-w-md overflow-scroll rounded-lg bg-white p-6">
-        <div className="mb-4 flex items-center justify-between">
-          <h3 className="text-outer_space-500 dark:text-platinum-500 text-lg font-semibold">
-            Create New Task
-          </h3>
-          <button
-            onClick={() => setIsOpen(false)}
-            className="hover:bg-platinum-500 dark:hover:bg-payne's_gray-400 rounded p-1"
-          >
-            <X size={20} />
-          </button>
+    <Modal
+      isOpen={isModalOpen}
+      setIsOpen={setIsModalOpen}
+      className="max-h-[700px] w-[700px]"
+      triggerComponent={<Button>Click Me</Button>}
+    >
+      <div className="mb-4 flex items-center justify-between">
+        <h3 className="text-lg font-semibold">Create New Task</h3>
+      </div>
+      <form ref={formRef} onSubmit={onSubmitHandler} className="space-y-4">
+        <div>
+          <label className="mb-2 block text-sm font-medium">Task Title *</label>
+          {/* TODO: add way to auto-focus title input */}
+          <Input {...register('title')} type="text" name="title" placeholder="Enter task title" />
+          <p className="mt-2 text-sm text-red-400">{errors.title?.message}</p>
         </div>
-
-        <form ref={formRef} onSubmit={onSubmitHandler} className="space-y-4">
-          <div>
-            <label className="text-outer_space-500 dark:text-platinum-500 mb-2 block text-sm font-medium">
-              Task Title *
-            </label>
-
-            {/* TODO: add way to auto-focus title input */}
-            <input
-              {...register('title')}
-              type="text"
-              name="title"
-              className="border-french_gray-300 dark:border-payne's_gray-400 dark:bg-outer_space-400 text-outer_space-500 dark:text-platinum-500 focus:ring-blue_munsell-500 w-full rounded-lg border bg-white px-3 py-2 focus:ring-2 focus:outline-hidden"
-              placeholder="Enter task title"
-            />
-            <p className="mt-2 text-sm text-red-400">{errors.title?.message}</p>
-          </div>
-
-          <div>
-            <label className="text-outer_space-500 dark:text-platinum-500 mb-2 block text-sm font-medium">
-              Description
-            </label>
-            <textarea
-              {...register('description')}
-              name="description"
-              rows={2}
-              className="border-french_gray-300 dark:border-payne's_gray-400 dark:bg-outer_space-400 text-outer_space-500 dark:text-platinum-500 focus:ring-blue_munsell-500 w-full rounded-lg border bg-white px-3 py-2 focus:ring-2 focus:outline-hidden"
-              placeholder="Concisely describe what the task is about"
-            />
-            <p className="mt-2 text-sm text-red-400">{errors.description?.message}</p>
-          </div>
-
-          {/* TOOD: make this a rich text editor */}
-          <div>
-            <label className="text-outer_space-500 dark:text-platinum-500 mb-2 block text-sm font-medium">
-              Task Detail
-            </label>
-            <textarea
-              {...register('detail')}
-              name="detail"
-              rows={3}
-              className="border-french_gray-300 dark:border-payne's_gray-400 dark:bg-outer_space-400 text-outer_space-500 dark:text-platinum-500 focus:ring-blue_munsell-500 w-full rounded-lg border bg-white px-3 py-2 focus:ring-2 focus:outline-hidden"
-              placeholder="More information about the task"
-            />
-            <p className="mt-2 text-sm text-red-400">{errors.description?.message}</p>
-          </div>
-          <div>
+        <div>
+          <label className="text-outer_space-500 dark:text-platinum-500 mb-2 block text-sm font-medium">
+            Description
+          </label>
+          <textarea
+            {...register('description')}
+            name="description"
+            rows={2}
+            className="focus:ring-visible border-border bg-input/30 w-full rounded-lg border px-3 py-2 text-sm focus:outline-hidden"
+            placeholder="Concisely describe what the task is about"
+          />
+          <p className="mt-2 text-sm text-red-400">{errors.description?.message}</p>
+        </div>
+        <section className="flex flex-row justify-between gap-2">
+          <div className="w-full">
             <label className="text-outer_space-500 dark:text-platinum-500 mb-2 block text-sm font-medium">
               Start Date
             </label>
-            <input
-              {...register('startDate', { setValueAs: (val) => (val === '' ? null : val) })}
-              type="date"
-              name="startDate"
-              className="border-french_gray-300 dark:border-payne's_gray-400 dark:bg-outer_space-400 text-outer_space-500 dark:text-platinum-500 focus:ring-blue_munsell-500 w-full rounded-lg border bg-white px-3 py-2 focus:ring-2 focus:outline-hidden"
-            />
-            <p className="mt-2 text-sm text-red-400">{errors.startDate?.message}</p>
-          </div>
-
-          <div>
-            <label className="text-outer_space-500 dark:text-platinum-500 mb-2 block text-sm font-medium">
-              Due Date
-            </label>
-            <input
+            <Input
               {...register('dueDate', { setValueAs: (val) => (val === '' ? null : val) })}
               type="date"
-              name="dueDate"
-              className="border-french_gray-300 dark:border-payne's_gray-400 dark:bg-outer_space-400 text-outer_space-500 dark:text-platinum-500 focus:ring-blue_munsell-500 w-full rounded-lg border bg-white px-3 py-2 focus:ring-2 focus:outline-hidden"
+              name="startDate"
             />
             <p className="mt-2 text-sm text-red-400">{errors.dueDate?.message}</p>
           </div>
-
-          <div className="flex w-full flex-row justify-between">
+          <div className="w-full">
             <label className="text-outer_space-500 dark:text-platinum-500 mb-2 block text-sm font-medium">
-              Priority
+              Due Date
             </label>
+            <Input
+              {...register('dueDate', { setValueAs: (val) => (val === '' ? null : val) })}
+              type="date"
+              name="dueDate"
+            />
+            <p className="mt-2 text-sm text-red-400">{errors.dueDate?.message}</p>
+          </div>
+          <div className="mt-2 flex w-full flex-row items-center gap-2">
+            <label className="text-sm font-medium">Priority</label>
             <select
               {...register('priority')}
               name="priority"
-              className="border-french_gray-300 dark:border-payne's_gray-400 dark:bg-outer_space-400 text-outer_space-500 dark:text-platinum-500 focus:ring-blue_munsell-500 rounded-lg border bg-white px-3 py-2 focus:ring-2 focus:outline-hidden"
+              className="bg-card rounded-lg border px-2 focus:outline-hidden"
             >
               {TASK_PRIORITY_VALUES.map((priority, index) => (
                 <option key={index} value={priority}>
-                  {priority}
+                  {capitalize(priority)}
                 </option>
               ))}
             </select>
           </div>
+        </section>
 
-          {/* TODO add assignees */}
-          <div>
-            <p className="text-outer_space-500 dark:text-platinum-500 mb-2 block text-sm font-medium">
-              Add assignees:
-            </p>
-            <p className="text-outer_space-500 dark:text-platinum-500 mb-2 block text-sm font-medium">
-              [DEFAULT]
-            </p>
-          </div>
-
-          {/* Server validation error messages */}
-          {/* TODO display all error messages not just one */}
-          <div>
-            {!errors && `CLIENT: ${JSON.stringify(errors, null, 2)}`}
-            {state?.success === false && (
-              <p className="mt-2 text-sm text-red-400">{`SERVER: ${state?.message}`}</p>
+        <div>
+          <label className="text-outer_space-500 dark:text-platinum-500 mb-2 block text-sm font-medium">
+            Task Detail
+          </label>
+          <Controller
+            control={control}
+            name="detail"
+            render={({ field: { onChange, value } }) => (
+              <MarkdownEditor value={value ?? ''} onValueChange={onChange} />
             )}
-          </div>
+          />
+          <p className="mt-2 text-sm text-red-400">{errors.description?.message}</p>
+        </div>
 
-          <div className="flex justify-end space-x-3 pt-4">
-            <button
-              type="button"
-              disabled={isPending}
-              onClick={() => setIsOpen(false)}
-              className="text-payne's_gray-500 dark:text-french_gray-400 hover:bg-platinum-500 dark:hover:bg-payne's_gray-400 rounded-lg px-4 py-2 transition-colors"
-            >
-              Cancel
-            </button>
-            <button
-              type="submit"
-              disabled={isPending}
-              className={`bg-blue_munsell-500 hover:bg-blue_munsell-600 rounded-lg px-4 py-2 text-white transition-colors ${isPending ? 'cursor-progress' : 'cursor-pointer'}`}
-            >
-              {isPending ? 'Creating' : `Create Task`}
-            </button>
+        {/* TODO add task assignees */}
+        <div>
+          <label className="mb-2 block text-sm font-medium">Task Labels</label>
+          <div className="flex flex-col gap-2">
+            {/* <Controller
+                  name="assigness"
+                  control={control}
+                  render={({ field }) => (
+                    <AddMemberMultiSelect
+                      value={field.value || []}
+                      onChange={field.onChange}
+                      fetchFunction={fetchUsersByEmail}
+                    />
+                  )}
+                /> */}
+            {/* <AddMemberMultiSelect
+              value={[]}
+              onChange={() => {}}
+              fetchFunction={async (input: string) => {
+                console.log(input);
+              }}
+            /> */}
           </div>
-        </form>
-      </div>
-    </div>
+        </div>
+        <div>
+          <label className="mb-2 block text-sm font-medium">Task Assignees</label>
+          <div className="flex flex-col gap-2">
+            {/* <Controller
+                  name="assigness"
+                  control={control}
+                  render={({ field }) => (
+                    <AddMemberMultiSelect
+                      value={field.value || []}
+                      onChange={field.onChange}
+                      fetchFunction={fetchUsersByEmail}
+                    />
+                  )}
+                /> */}
+            {/* <AddMemberMultiSelect
+              value={[]}
+              onChange={() => {}}
+              fetchFunction={async (input: string) => {
+                console.log(input);
+              }}
+            /> */}
+          </div>
+        </div>
+        {/* Server validation error messages */}
+        {/* TODO display all error messages not just one */}
+        <div>
+          {!errors && `CLIENT: ${JSON.stringify(errors, null, 2)}`}
+          {state?.success === false && (
+            <p className="mt-2 text-sm text-red-400">{`SERVER: ${state?.message}`}</p>
+          )}
+        </div>
+        <div className="z-1 flex justify-end space-x-3 border-red-500 pt-4">
+          <DialogClose asChild>
+            <Button type="button" disabled={isPending} variant={'cancel'}>
+              Cancel
+            </Button>
+          </DialogClose>
+          <Button
+            type="submit"
+            disabled={isPending}
+            className={`${isPending ? 'cursor-progress' : 'cursor-pointer'}`}
+          >
+            {isPending ? 'Creating' : `Create Task`}
+          </Button>
+        </div>
+      </form>
+    </Modal>
   );
 }
