b3ae8327c647ba8cc30c078b56d53a6f615e202a
jldavid-strat <i_jed.david@stratpoint.com>
2025-08-29 16:30:08 +0800

    backend(column-action): refactor fetching logic and added way to determine db context

--- actions/kanban_column.actions.ts
+++ actions/kanban_column.actions.ts
@@ -1,40 +1,36 @@
 'use server';
+import { DEFAULT_COLUMN_NAMES, DEFAULT_COLUMNS_DATA } from '@/constants/columns';
 import { ACTIONS, RESOURCES } from '@/constants/permissions';
-import { db } from '@/lib/db/connect_db';
+import { db, DBTransaction } from '@/lib/db/connect_db';
 import { kanbanColumns, projectKanbanColumns } from '@/lib/db/schema/schema';
 import { serverEvents } from '@/lib/events/event-emitter';
-import {
-  getKanbanColumnByName,
-  getKanbanColumnsByProjectId,
-} from '@/lib/queries/kanban_column.queries';
+import { getKanbanColumnsByProjectId } from '@/lib/queries/kanban_column.queries';
 import { checkMemberPermission } from '@/lib/queries/permssions.queries';
 import { getCurrentUserId } from '@/lib/queries/user.queries';
 import { KanbanColumn, Project } from '@/types/db.types';
 import { ReorderColumnDataType } from '@/types/types';
 import { and, eq, inArray, sql, SQL } from 'drizzle-orm';
 
-export async function createDefaultKanbanColumns(projectId: Project['id']) {
+export async function createDefaultKanbanColumns(
+  projectId: Project['id'],
+  dbTransaction?: DBTransaction,
+) {
   try {
-    const { 0: backlog, 1: completed } = await Promise.all([
-      getKanbanColumnByName('Backlog'),
-      getKanbanColumnByName('Completed'),
-    ]);
+    const dbContext = dbTransaction ?? db;
+    const defaultColumns = await dbContext
+      .select()
+      .from(kanbanColumns)
+      .where(inArray(kanbanColumns.name, DEFAULT_COLUMN_NAMES));
 
-    await db.insert(projectKanbanColumns).values([
-      {
-        kanbanColumnId: backlog.id,
-        projectId: projectId,
-        description: "Tasks that hasn't been started",
-        position: 0,
-        isCustom: false,
-      },
-      {
-        kanbanColumnId: completed.id,
-        projectId: projectId,
-        position: 1,
-        isCustom: false,
-      },
-    ]);
+    const toInsertDefaultColumns = defaultColumns.map((col) => ({
+      kanbanColumnId: col.id,
+      projectId: projectId,
+      description: DEFAULT_COLUMNS_DATA[col.name].description,
+      position: DEFAULT_COLUMNS_DATA[col.name].defaultPosition,
+      isCustom: false,
+    }));
+
+    await dbContext.insert(projectKanbanColumns).values(toInsertDefaultColumns);
     return {
       success: true,
       message: 'Successfully created default kanban columns',
