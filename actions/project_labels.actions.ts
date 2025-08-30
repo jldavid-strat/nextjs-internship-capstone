d20c79e73250bece89d87f5e82e3846d3ac25700
jldavid-strat <i_jed.david@stratpoint.com>
2025-08-30 23:53:07 +0800

    backend(project-labels-actions): made update ande delete label server actions

--- actions/project_labels.actions.ts
+++ actions/project_labels.actions.ts
@@ -1,17 +1,19 @@
 'use server';
-import { UnauthorizedError } from '@/constants/error';
+import { DatabaseOperationError, UnauthorizedError } from '@/constants/error';
 import { DEFAULT_LABELS, defaultLabelNames } from '@/constants/labels';
 import { ACTIONS, RESOURCES } from '@/constants/permissions';
 import { db, DBTransaction } from '@/lib/db/connect_db';
 import { labels, projectLabels } from '@/lib/db/schema/schema';
 import { checkMemberPermission } from '@/lib/queries/permssions.queries';
-import { getProjectLabelByName } from '@/lib/queries/project_labels.queries';
+import { getProjectLabelById, getProjectLabelByName } from '@/lib/queries/project_labels.queries';
 import { getCurrentUserId } from '@/lib/queries/user.queries';
+import getDataDiff from '@/lib/utils/data_diff';
 import { getErrorMessage } from '@/lib/utils/error.utils';
-import { ProjectLabelSchema } from '@/lib/validations/project-label.validations';
+import { DEFAULT_COLOR, ProjectLabelSchema } from '@/lib/validations/project-label.validations';
 import { Project, ProjectLabel } from '@/types/db.types';
 import { ActionResult } from '@/types/types';
-import { inArray } from 'drizzle-orm';
+import { and, eq, inArray } from 'drizzle-orm';
+import { revalidatePath } from 'next/cache';
 
 export async function createDefaultProjectLabels(
   projectId: Project['id'],
@@ -46,11 +48,11 @@ export async function createDefaultProjectLabels(
   }
 }
 
-export async function addProjectLabels(
+export async function addProjectLabel(
   projectId: Project['id'],
   previousState: unknown,
   projectLabelData: FormData,
-) {
+): Promise<ActionResult> {
   try {
     const currentUserId = await getCurrentUserId();
 
@@ -92,11 +94,106 @@ export async function addProjectLabels(
       projectId: projectId,
     });
 
+    revalidatePath(`/projects/${projectId}/settings`);
+
     return {
       success: true,
     };
   } catch (error) {
     console.error(error);
+    return {
+      success: false,
+      error: getErrorMessage(error),
+    };
+  }
+}
+
+export async function updateProjectLabel(
+  additionalInfo: { projectId: Project['id']; labelId: ProjectLabel['labelId'] },
+  previousState: unknown,
+  projectLabelData: FormData,
+): Promise<ActionResult> {
+  try {
+    const { projectId, labelId } = additionalInfo;
+    const currentUserId = await getCurrentUserId();
+
+    const { isAuthorize } = await checkMemberPermission(
+      currentUserId,
+      projectId,
+      RESOURCES.PROJECT_LABELS,
+      ACTIONS.UPDATE,
+    );
+
+    if (!isAuthorize)
+      throw new UnauthorizedError('User is not authorized to update project labels');
+
+    const originalProjectLabelData = await getProjectLabelById(labelId, projectId);
+
+    if (!originalProjectLabelData)
+      throw new DatabaseOperationError('Something went wrong. Please try again');
+
+    const originalData = {
+      name: originalProjectLabelData.name,
+      color: originalProjectLabelData.color ?? DEFAULT_COLOR,
+    };
+
+    const validatedData = ProjectLabelSchema.parse({
+      name: projectLabelData.get('name') as string,
+      color: projectLabelData.get('color') as string,
+    });
+
+    const changes = getDataDiff(originalData, validatedData);
+
+    console.log(changes);
+
+    if (changes === null) throw new Error('No changes made');
+
+    if (changes.name) {
+      // update the name in label tabel
+      await db
+        .update(labels)
+        .set({
+          name: validatedData.name,
+        })
+        .where(eq(labels.id, labelId));
+    }
+
+    // if not update the color in project label table
+    await db
+      .update(projectLabels)
+      .set({
+        color: validatedData.color,
+      })
+      .where(eq(projectLabels.labelId, labelId));
+
+    revalidatePath(`/projects/${projectId}/settings`);
+
+    return {
+      success: true,
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      success: false,
+      error: getErrorMessage(error),
+    };
+  }
+}
+
+export async function deleteProjectLabel(
+  labelId: ProjectLabel['labelId'],
+  projectId: Project['id'],
+): Promise<ActionResult> {
+  try {
+    await db
+      .delete(projectLabels)
+      .where(and(eq(projectLabels.labelId, labelId), eq(projectLabels.labelId, labelId)));
+    revalidatePath(`/projects/${projectId}/settings`);
+
+    return {
+      success: true,
+    };
+  } catch (error) {
     return {
       success: false,
       error: getErrorMessage(error),
