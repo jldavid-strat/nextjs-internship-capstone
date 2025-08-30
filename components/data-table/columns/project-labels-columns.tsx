95f63a768545a720d1c10292cebb8d423094531c
jldavid-strat <i_jed.david@stratpoint.com>
2025-08-30 23:54:02 +0800

    modify(project-label-column): modify edit button to modal button trigger and added isPending state to both buttons

--- components/data-table/columns/project-labels-columns.tsx
+++ components/data-table/columns/project-labels-columns.tsx
@@ -1,7 +1,12 @@
+'use client';
+
 import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
-import { ColumnDef } from '@tanstack/react-table';
+import { ColumnDef, Row } from '@tanstack/react-table';
 import { ProjectLabelTableData } from '../project-label-table';
+import EditProjectLabelModal from '@/components/modals/edit-project-label-modal';
+import { deleteProjectLabel } from '@/actions/project_labels.actions';
+import { useState } from 'react';
 
 export const projectLabelColumns = (canMutate: boolean): ColumnDef<ProjectLabelTableData>[] => [
   {
@@ -24,21 +29,48 @@ export const projectLabelColumns = (canMutate: boolean): ColumnDef<ProjectLabelT
 const actionColumn: ColumnDef<ProjectLabelTableData>[] = [
   {
     id: 'actions',
-    cell: ({ row }) => {
-      const label = row.original;
-      return (
-        <div className="flex flex-row items-center justify-center gap-2">
-          <Button variant="outline" onClick={() => console.log('Edit:', label.labelId)}>
-            Edit
-          </Button>
-          <Button
-            className="border-destructive/60 dark:text-foreground border-1 bg-red-500/20 text-red-500 hover:bg-red-500/30"
-            onClick={() => console.log('Delete:', label.labelId)}
-          >
-            Delete
-          </Button>
-        </div>
-      );
-    },
+    cell: ({ row }) => <ActionCell row={row} />,
   },
 ];
+
+function ActionCell({ row }: { row: Row<ProjectLabelTableData> }) {
+  const [isPending, setIsPending] = useState<boolean>(false);
+  const label = row.original;
+
+  const handleDelete = async (labelId: ProjectLabelTableData['labelId']) => {
+    setIsPending(true);
+    console.log('Delete:', labelId);
+    const { success } = await deleteProjectLabel(label.labelId, label.projectId);
+
+    setIsPending(false);
+
+    // show toast
+    if (!success) alert('did not delete');
+    alert('did delete');
+    // call delete project label server action
+  };
+
+  return (
+    <div className="flex flex-row items-center justify-center gap-2">
+      {/* open edit project label modal */}
+      <EditProjectLabelModal
+        projectLabelData={{
+          labelId: label.labelId,
+          name: label.labelName,
+          projectId: label.projectId,
+          color: label.color,
+        }}
+        disabled={isPending}
+      />
+
+      <Button
+        className="border-destructive/60 dark:text-foreground border-1 bg-red-500/20 text-red-500 hover:bg-red-500/30"
+        onClick={() => {
+          handleDelete(label.labelId);
+        }}
+      >
+        {isPending ? 'Deleting' : 'Delete'}
+      </Button>
+    </div>
+  );
+}
