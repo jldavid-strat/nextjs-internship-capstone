08bb3036bfeccee421ed71ab66dfb1b8b7761e52
jldavid-strat <i_jed.david@stratpoint.com>
2025-08-30 22:28:56 +0800

    backend(project-label-action): implemented add project-label server actions

--- actions/project_labels.actions.ts
+++ actions/project_labels.actions.ts
@@ -1,7 +1,14 @@
 'use server';
+import { UnauthorizedError } from '@/constants/error';
 import { DEFAULT_LABELS, defaultLabelNames } from '@/constants/labels';
+import { ACTIONS, RESOURCES } from '@/constants/permissions';
 import { db, DBTransaction } from '@/lib/db/connect_db';
 import { labels, projectLabels } from '@/lib/db/schema/schema';
+import { checkMemberPermission } from '@/lib/queries/permssions.queries';
+import { getProjectLabelByName } from '@/lib/queries/project_labels.queries';
+import { getCurrentUserId } from '@/lib/queries/user.queries';
+import { getErrorMessage } from '@/lib/utils/error.utils';
+import { ProjectLabelSchema } from '@/lib/validations/project-label.validations';
 import { Project, ProjectLabel } from '@/types/db.types';
 import { ActionResult } from '@/types/types';
 import { inArray } from 'drizzle-orm';
@@ -35,6 +42,64 @@ export async function createDefaultProjectLabels(
     return {
       success: false,
       error: 'Failed to initialize default kanban columns',
+    };
+  }
+}
+
+export async function addProjectLabels(
+  projectId: Project['id'],
+  previousState: unknown,
+  projectLabelData: FormData,
+) {
+  try {
+    const currentUserId = await getCurrentUserId();
+
+    const { isAuthorize } = await checkMemberPermission(
+      currentUserId,
+      projectId,
+      RESOURCES.PROJECT_LABELS,
+      ACTIONS.CREATE,
+    );
+
+    if (!isAuthorize) throw new UnauthorizedError('User is not authorized to add project labels');
+
+    const validatedData = ProjectLabelSchema.parse({
+      name: projectLabelData.get('name') as string,
+      color: projectLabelData.get('color') as string,
+    });
+
+    // check if label already exist
+    const existingLabel = await getProjectLabelByName(validatedData.name);
+
+    // insert new label name if it does not exist
+    let labelId = existingLabel === undefined ? null : existingLabel.id;
+    if (!labelId) {
+      const [labelResult] = await db
+        .insert(labels)
+        .values({
+          name: validatedData.name,
+        })
+        .returning({ id: labels.id });
+
+      labelId = labelResult.id;
+    }
+
+    // insert into list of label in current project
+    await db.insert(projectLabels).values({
+      isCustom: true,
+      labelId: labelId,
+      color: validatedData.color.toUpperCase(),
+      projectId: projectId,
+    });
+
+    return {
+      success: true,
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      success: false,
+      error: getErrorMessage(error),
     };
   }
 }
