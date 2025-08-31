56b6c183ef0098fcab2e14de80393aaecdc4d47b
jldavid-strat <i_jed.david@stratpoint.com>
2025-08-31 14:21:42 +0800

    chore: update member multiselect import

--- components/ui/add-member-multiselect.tsx
+++ components/ui/add-member-multiselect.tsx
@@ -2,12 +2,11 @@
 
 import { useState, useEffect, useRef, useMemo, useCallback } from 'react';
 import { X } from 'lucide-react';
-import Image from 'next/image';
 import { User } from '@/types/db.types';
 import { Table, TableBody, TableRow, TableCell } from './table';
 import { SELECT_ROLE_VALUES } from '@/lib/db/schema/enums';
 import { capitalize } from 'lodash';
-import { UserMultiSelect } from './user-multi-select';
+import { UserMultiSelect } from './multi-select';
 import { Avatar, AvatarFallback, AvatarImage } from './avatar';
 
 export type MemberValue = Array<{ userId: string; role: string }>;
