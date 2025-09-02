b1a11a72ec004b0552685f91b69bde6aa387478e
jldavid-strat <i_jed.david@stratpoint.com>
2025-09-02 22:49:53 +0800

    backend(action): implemented server actions for task comment

--- actions/task_comment.actions.ts
+++ actions/task_comment.actions.ts
@@ -1,47 +1,93 @@
 'use server';
-import { ActionResult } from '@/types';
+import { ActionResult, QueryResult } from '@/types/types';
 import { db } from '@/lib/db/connect_db';
 import { taskComments } from '@/lib/db/schema/schema';
 import { eq } from 'drizzle-orm';
-import { TaskComment, CreateTaskComment, UpdateTaskComment } from '@/types/db.types';
+import { TaskComment, Project, Task, TaskCommentQuery } from '@/types/db.types';
+import { getErrorMessage } from '@/lib/utils/error.utils';
+import { checkMemberPermission } from '@/lib/queries/permssions.queries';
+import { ACTIONS, RESOURCES } from '@/constants/permissions';
+import { UnauthorizedError } from '@/constants/error';
+import { getCurrentUserId } from '@/lib/queries/user.queries';
+import { TaskCommentSchema } from '@/lib/validations/task.validations';
+import { users } from '@/migrations/schema';
 
-export async function createTask(taskCommentData: CreateTaskComment): Promise<ActionResult> {
+export async function addTaskComment(
+  taskId: Task['id'],
+  projectId: Project['id'],
+  previosState: unknown,
+  taskCommentData: FormData,
+): Promise<ActionResult> {
   try {
-    await db.insert(taskComments).values({
-      authorId: taskCommentData.authorId,
-      taskId: taskCommentData.taskId,
-      content: taskCommentData.content,
-      createdAt: new Date(),
+    const currentUserId = await getCurrentUserId();
+
+    const { isAuthorize } = await checkMemberPermission(
+      currentUserId,
+      projectId,
+      RESOURCES.TASK_COMMENTS,
+      ACTIONS.CREATE,
+    );
+
+    if (!isAuthorize) throw new UnauthorizedError('User is unauthorized to add task comments');
+
+    const validatedData = TaskCommentSchema.parse({
+      content: taskCommentData.get('content') as string,
+      projectId: projectId,
+      taskId: taskId,
+      authorId: currentUserId as string,
     });
-    return { success: true, message: `Task Comment successfully created` };
+
+    await db.insert(taskComments).values({
+      ...validatedData,
+    });
+    return { success: true };
   } catch (error) {
     return {
       success: false,
-      message: `Failed to create task comment. Error ${error}`,
-      error: JSON.stringify(error),
+      error: getErrorMessage(error),
     };
   }
 }
 
 export async function updateTaskComment(
+  taskId: Task['id'],
   taskCommentId: TaskComment['id'],
-  taskCommentData: UpdateTaskComment,
+  projectId: Project['id'],
+  previosState: unknown,
+  taskCommentData: FormData,
 ): Promise<ActionResult> {
   try {
+    const currentUserId = await getCurrentUserId();
+
+    const { isAuthorize } = await checkMemberPermission(
+      currentUserId,
+      projectId,
+      RESOURCES.TASK_COMMENTS,
+      ACTIONS.UPDATE,
+    );
+
+    if (!isAuthorize) throw new UnauthorizedError('User is unauthorized to update task comments');
+
+    const validatedData = TaskCommentSchema.parse({
+      content: taskCommentData.get('content') as string,
+      projectId: projectId,
+      taskId: taskId,
+      authorId: currentUserId as string,
+    });
+
     await db
       .update(taskComments)
       .set({
-        content: taskCommentData.content,
+        ...validatedData,
         updatedAt: new Date(),
       })
       .where(eq(taskComments.id, taskCommentId));
 
-    return { success: true, message: `Task comment successfully updated` };
+    return { success: true };
   } catch (error) {
     return {
       success: false,
-      message: `Failed to update task comment`,
-      error: JSON.stringify(error),
+      error: getErrorMessage(error),
     };
   }
 }
@@ -49,46 +95,42 @@ export async function deleteTaskComment(taskCommentId: TaskComment['id']): Promi
   try {
     await db.delete(taskComments).where(eq(taskComments.id, taskCommentId));
 
-    return { success: true, message: `Task comment successfully deleted` };
+    return { success: true };
   } catch (error) {
     return {
       success: false,
-      message: `Failed to delete task comment`,
-      error: JSON.stringify(error),
+      error: getErrorMessage(error),
     };
   }
 }
 
-// TODO: retrieve all comments from a specific task
-// export async function getTaskComments(
-//   taskId: SelectTask['id'],
-//   taskCommentId: SelectTaskComment['id'],
-// ): Promise<queryResult<SelectTaskComment[]>> {
-//   try {
-//     // const taskComments = await db
-//     //   .select()
-//     //   .from(taskComment)
-//     //   .innerJoin(task, eq(taskId, taskCommentId))
-//     //   .where(eq());
-//     const taskComments = await db.query.task.findMany({
-//       where: eq(task.id, taskId),
-//       with: {
-//         taskComment: {
-//           where: (taskComment, { eq }) => eq(taskComment., taskCommentId),
-//         },
-//       },
-//     });
+export async function getTaskComments(
+  taskId: Task['id'],
+): Promise<QueryResult<TaskCommentQuery[]>> {
+  try {
+    const taskCommentList = await db
+      .select({
+        taskCommentId: taskComments.id,
+        authorId: users.id,
+        firstName: users.firstName,
+        lastName: users.lastName,
+        primaryEmailAdress: users.primaryEmailAddress,
+        userImgLink: users.imgUrl,
+        content: taskComments.content,
+        createdAt: taskComments.createdAt,
+      })
+      .from(taskComments)
+      .innerJoin(users, eq(users.id, taskComments.authorId))
+      .where(eq(taskComments.taskId, taskId));
 
-//     return {
-//       success: true,
-//       message: `Task comment successfully retrieved`,
-//       data: taskComments,
-//     };
-//   } catch (error) {
-//     return {
-//       success: false,
-//       message: `Failed to delete task comment`,
-//       error: JSON.stringify(error),
-//     };
-//   }
-// }
+    return {
+      success: true,
+      data: taskCommentList,
+    };
+  } catch (error) {
+    return {
+      success: false,
+      error: getErrorMessage(error),
+    };
+  }
+}
