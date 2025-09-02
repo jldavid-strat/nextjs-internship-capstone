c29e1f7539859455408ee9de3bd3b1d5371561c3
jldavid-strat <i_jed.david@stratpoint.com>
2025-09-02 22:45:16 +0800

    feat(hook): added custom hook for fetching task comments

---
+++ hooks/use-task-comments.ts
@@ -0,0 +1,31 @@
+'use client';
+
+import { getTaskComments } from '@/actions/task_comment.actions';
+import { Task, TaskCommentQuery } from '@/types/db.types';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+
+type useTasksCommentsResult = {
+  taskComments: TaskCommentQuery[];
+  isTaskCommentLoading: boolean;
+  error: unknown;
+  isTaskCommentSuccess: boolean;
+};
+
+export function useTaskComments(taskId: Task['id']): useTasksCommentsResult {
+  // query for getting task comments
+  const { data, isLoading, error, isSuccess } = useQuery<TaskCommentQuery[]>({
+    queryKey: ['task-comments', taskId],
+    queryFn: async () => {
+      const res = await getTaskComments(taskId);
+      if (!res.success) throw new Error(JSON.stringify(res.error));
+      return res.data;
+    },
+  });
+
+  return {
+    taskComments: data ?? [],
+    isTaskCommentLoading: isLoading,
+    isTaskCommentSuccess: isSuccess,
+    error: error,
+  };
+}
