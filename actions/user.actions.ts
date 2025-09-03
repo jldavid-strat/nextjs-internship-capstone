beafcb788f4ac1ee47af01336a828f1e28a3b95d
jldavid-strat <i_jed.david@stratpoint.com>
2025-09-03 20:05:53 +0800

    modify(actions): modify user mutations to support webhook data

--- actions/user.actions.ts
+++ actions/user.actions.ts
@@ -1,57 +1,125 @@
 'use server';
+import { NotFoundError } from '@/constants/error';
 import { db } from '@/lib/db/connect_db';
 import { users } from '@/lib/db/schema/schema';
-import { CreateUser, UpdateUser, User } from '@/types/db.types';
-import { auth } from '@clerk/nextjs/server';
+import { getUserByClerkId } from '@/lib/queries/user.queries';
+import { getErrorMessage } from '@/lib/utils/error.utils';
+import { EditUserSchema, UserSchema } from '@/lib/validations/user.validations';
+import { User } from '@/types/db.types';
+import { ActionResult } from '@/types/types';
+import { auth, DeletedObjectJSON, UserJSON } from '@clerk/nextjs/server';
 import { and, eq, ilike, notInArray } from 'drizzle-orm';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import 'server-only';
 
-export async function createUser(clerkUser: Omit<CreateUser, 'updatedAt'>): Promise<void> {
+export async function createUser(eventData: UserJSON): Promise<ActionResult> {
   try {
-    await db.insert(users).values({
-      clerkId: clerkUser.clerkId,
-      firstName: clerkUser.firstName,
-      lastName: clerkUser.lastName,
-      imgLink: clerkUser.imgLink,
-      primaryEmailAddress: clerkUser.primaryEmailAddress,
-      createdAt: clerkUser.createdAt,
+    const clerkId = eventData.id;
+
+    // check if clerkId already exists
+    const { success } = await getUserByClerkId(clerkId);
+
+    // throw error if id is already in the database
+    if (success) throw new Error('User already exists in the database');
+
+    const primaryEmailObj = eventData.email_addresses.find(
+      (e) => e.id === eventData.primary_email_address_id,
+    );
+
+    if (eventData.primary_email_address_id === null || primaryEmailObj === undefined)
+      throw new NotFoundError('Primary Email ID is null or undefined');
+
+    const validatedData = UserSchema.parse({
+      clerkId: clerkId,
+      firstName: eventData.first_name,
+      lastName: eventData.last_name,
+      imgLink: eventData.image_url,
+      primaryEmailAddress: primaryEmailObj.email_address,
+      createdAt: new Date(eventData.created_at),
+      updatedAt: new Date(eventData.updated_at),
     });
+
+    await db.insert(users).values(validatedData);
+
+    return {
+      success: true,
+    };
   } catch (error) {
     console.error(error);
+    return {
+      success: false,
+      error: getErrorMessage(error),
+    };
   }
 }
 
-export async function updatedUser(
-  clerkUserId: User['clerkId'],
-  clerkUser: UpdateUser,
-): Promise<void> {
+export async function updateUser(eventData: UserJSON): Promise<ActionResult> {
   try {
-    await db
-      .update(users)
-      .set({
-        firstName: clerkUser.firstName,
-        lastName: clerkUser.lastName,
-        imgLink: clerkUser.imgLink,
-        primaryEmailAddress: clerkUser.primaryEmailAddress,
-        updatedAt: clerkUser.updatedAt,
-      })
-      .where(eq(users.clerkId, clerkUserId));
+    const clerkId = eventData.id;
+
+    // check if clerkId already exists
+    const { success } = await getUserByClerkId(clerkId);
+
+    // check if user exists in db
+    if (!success) throw new Error('User does not exist in the database');
+
+    const primaryEmailObj = eventData.email_addresses.find(
+      (e) => e.id === eventData.primary_email_address_id,
+    );
+
+    if (eventData.primary_email_address_id === null || primaryEmailObj === undefined)
+      throw new NotFoundError('Primary Email ID is null or undefined');
+
+    const currentUserData = {
+      clerkId: clerkId,
+      firstName: eventData.first_name,
+      lastName: eventData.last_name,
+      imgLink: eventData.image_url,
+      primaryEmailAddress: primaryEmailObj.email_address,
+      updatedAt: new Date(eventData.updated_at),
+    };
+
+    const validatedData = EditUserSchema.parse({ ...currentUserData });
+
+    await db.update(users).set(validatedData).where(eq(users.clerkId, clerkId));
 
     // invalidate cache on settings page to load new user info
     revalidatePath(`/(dashboard)`);
+    return {
+      success: true,
+    };
   } catch (error) {
     console.error(error);
+    return {
+      success: false,
+      error: getErrorMessage(error),
+    };
   }
 }
 
-export async function deleteUser(clerkId: User['clerkId']): Promise<void> {
+export async function deleteUser(eventData: DeletedObjectJSON): Promise<ActionResult> {
   try {
+    const clerkId = eventData.id;
+
+    if (!clerkId) throw new Error('Clerk ID received is null or undefined');
+
+    // check if clerkId already exists
+    const { success } = await getUserByClerkId(clerkId);
+
+    // check if user exists in db
+    if (!success) throw new Error('User does not exist in the database');
+
     await db.delete(users).where(eq(users.clerkId, clerkId));
-    redirect('/sign-up');
+    return {
+      success: true,
+    };
   } catch (error) {
     console.error(error);
+    return {
+      success: false,
+      error: getErrorMessage(error),
+    };
   }
 }
 
