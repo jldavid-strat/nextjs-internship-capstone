26368ad60a28c195878b5f16716bf41c31832aa5
jldavid-strat <i_jed.david@stratpoint.com>
2025-09-03 17:18:07 +0800

    chore: update project page file

--- app/(dashboard)/projects/page.tsx
+++ app/(dashboard)/projects/page.tsx
@@ -1,7 +1,9 @@
-import { CreateProjectButton } from '@/components/buttons/create-project-button';
 import { getCurrentUserId } from '@/lib/queries/user.queries';
-import ProjectList from '@/components/project-list';
+import ProjectList from '@/components/project/project-list';
 import { getAllUserProject } from '@/lib/queries/project.queries';
+import { Button } from '@/components/ui/button';
+import { redirect } from 'next/navigation';
+import { Plus } from 'lucide-react';
 
 export default async function ProjectsPage() {
   const currentUserId = await getCurrentUserId();
@@ -17,7 +19,13 @@ export default async function ProjectsPage() {
             Manage and organize your team projects
           </p>
         </div>
-        <CreateProjectButton />
+        <Button
+          onClick={() => redirect('/projects/create')}
+          className="bg-blue_munsell-500 hover:bg-blue_munsell-600 inline-flex items-center rounded-lg px-4 py-2 text-white transition-colors"
+        >
+          <Plus size={20} className="mr-2" />
+          New Project
+        </Button>
       </div>
 
       {/* project grid */}
