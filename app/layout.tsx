de433067b57cc7413a2763bb7cfebcc8e1e250dc
jldavid-strat <i_jed.david@stratpoint.com>
2025-09-03 16:40:01 +0800

    chore: update import path for global.css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type React from 'react';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import './globals.css';
+import '@/styles/globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
 import { ThemeProvider } from '@/providers/theme-provider';
 import Providers from '@/providers/query-provider';
