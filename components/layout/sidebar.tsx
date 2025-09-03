58fe220bc0bdbe024519161a4b2cacc3bdd8905b
jldavid-strat <i_jed.david@stratpoint.com>
2025-09-03 11:34:52 +0800

    feat(nav-bar): added active style for navigation links

--- components/layout/sidebar.tsx
+++ components/layout/sidebar.tsx
@@ -1,24 +1,14 @@
 'use client';
 
 import Link from 'next/link';
-import {
-  Home,
-  FolderOpen,
-  Users,
-  Settings,
-  X,
-  BarChart3,
-  Calendar,
-  Menu,
-  PanelRightClose,
-  PanelLeftClose,
-} from 'lucide-react';
-import { ThemeToggle } from '../theme/theme-toggle';
+import { Home, FolderOpen, Settings, X, BarChart3, Calendar } from 'lucide-react';
+import { usePathname } from 'next/navigation';
+import path from 'path';
+import { cn } from '@/lib/utils/shadcn-utils';
 
 const navigation = [
   { name: 'Dashboard', href: '/dashboard', icon: Home },
   { name: 'Projects', href: '/projects', icon: FolderOpen },
-  { name: 'Team', href: '/team', icon: Users },
   { name: 'Analytics', href: '/analytics', icon: BarChart3 },
   { name: 'Calendar', href: '/calendar', icon: Calendar },
   { name: 'Settings', href: '/settings', icon: Settings },
@@ -30,6 +20,9 @@ type SideBarProps = {
 };
 
 export default function Sidebar({ sideBarOpen, setSideBarOpen }: SideBarProps) {
+  const pathName = usePathname();
+
+  console.log(pathName);
   return (
     <>
       {/* Mobile sidebar overlay */}
@@ -40,18 +33,12 @@ export default function Sidebar({ sideBarOpen, setSideBarOpen }: SideBarProps) {
         />
       )}
       <div
-        className={`border-border fixed inset-y-0 left-0 z-50 w-64 transform border-r bg-black/20 transition-transform duration-300 ease-in-out lg:translate-x-0 ${sideBarOpen ? 'translate-x-0' : '-translate-x-full'}`}
+        className={`border-border bg-sidebar fixed inset-y-0 left-0 z-50 w-64 transform border-r transition-transform duration-300 ease-in-out lg:translate-x-0 ${sideBarOpen ? 'translate-x-0' : '-translate-x-full'}`}
       >
         <div className="flex h-16 items-center justify-between px-6">
           <Link href="/" className="text-primary text-2xl font-bold">
             TaskFlow
           </Link>
-          {/* <button
-            onClick={() => setSideBarOpen(false)}
-            className="hover:bg-accent rounded-lg p-2 md:visible"
-          >
-            <PanelLeftClose size={20} />
-          </button> */}
           <button
             onClick={() => setSideBarOpen(false)}
             className="hover:bg-accent rounded-lg p-2 lg:hidden"
@@ -66,7 +53,10 @@ export default function Sidebar({ sideBarOpen, setSideBarOpen }: SideBarProps) {
               <li key={item.name}>
                 <Link
                   href={item.href}
-                  className="hover:bg-accent flex items-center rounded-lg px-3 py-2 text-sm font-medium transition-colors"
+                  className={cn(
+                    'hover:bg-accent flex items-center rounded-lg px-3 py-2 text-sm font-medium transition-colors',
+                    pathName.includes(item.href) && 'bg-primary',
+                  )}
                 >
                   <item.icon className="mr-3" size={20} />
                   {item.name}
