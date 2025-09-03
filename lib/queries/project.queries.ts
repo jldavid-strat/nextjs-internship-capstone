4d7d5e0cb795ce8aada35a40405a438e61947f55
jldavid-strat <i_jed.david@stratpoint.com>
2025-09-03 11:30:44 +0800

    modify(query): implemented getProjectHeader query

--- lib/queries/project.queries.ts
+++ lib/queries/project.queries.ts
@@ -1,7 +1,7 @@
 import 'server-only';
-import { eq } from 'drizzle-orm';
+import { and, countDistinct, eq, sql } from 'drizzle-orm';
 import { db } from '../db/connect_db';
-import { projects } from '../db/schema/schema';
+import { projects, tasks } from '../db/schema/schema';
 import { QueryResult } from '@/types/types';
 import { Project } from '@/types/db.types';
 import { projectMembers, users } from '@/migrations/schema';
@@ -9,6 +9,9 @@ import { getProjectMembers } from '@/actions/project_member.actions';
 import { getProjectTeams } from './project_teams.queries';
 import { getErrorMessage } from '../utils/error.utils';
 import { getProjectLabels } from '@/actions/project_labels.actions';
+import { getMemberCount } from './project_member.queries';
+import { getCompletedTaskCount, getTotalTaskCount } from './task.queries';
+import { getCurrentUserId } from './user.queries';
 
 // TODO [CONSIDER] check read authorization in fetching projects
 
@@ -31,8 +34,16 @@ export async function getAllProjects(): Promise<QueryResult<Project[]>> {
 }
 
 export async function getAllUserProject(userId: string) {
-  // TODO: add indexes to the identifiers used in this query
   try {
+    const getProgressCase = sql<number>`
+      CASE 
+        WHEN COUNT(${tasks.id}) = 0 THEN 0
+        ELSE ROUND(
+          (SUM(CASE WHEN ${tasks.isCompleted} = true THEN 1 ELSE 0 END)::decimal / COUNT(${tasks.id})) * 100
+        )
+      END
+    `;
+
     const projectList = await db
       .select({
         id: projects.id,
@@ -40,16 +51,19 @@ export async function getAllUserProject(userId: string) {
         description: projects.description,
         status: projects.status,
         dueDate: projects.dueDate,
+        memberCount: countDistinct(projectMembers.userId),
+        progress: getProgressCase,
       })
       .from(projects)
       .leftJoin(projectMembers, eq(projectMembers.projectId, projects.id))
-      .leftJoin(users, eq(users.id, projectMembers.userId))
-      .where(eq(users.id, userId));
+      .leftJoin(users, eq(users.id, userId))
+      .leftJoin(tasks, eq(tasks.projectId, projects.id))
+      .where(eq(projects.id, projects.id))
+      .groupBy(projects.id);
 
     if (projectList.length === 0) {
       throw new Error('No projects found');
     }
-
     return {
       success: true,
       data: projectList,
@@ -63,6 +77,20 @@ export async function getAllUserProject(userId: string) {
   }
 }
 
+// gets progress in percentage
+export async function getProjectProgress(projectId: Project['id']) {
+  try {
+    const completedTaskCount = await getCompletedTaskCount(projectId);
+    const totalTaskCount = await getTotalTaskCount(projectId);
+
+    if (!completedTaskCount || !totalTaskCount) return 0;
+
+    return (totalTaskCount / completedTaskCount) * 100;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 export async function getProjectById(projectId: Project['id']) {
   try {
     const singularProject = await db.select().from(projects).where(eq(projects.id, projectId));
@@ -70,6 +98,54 @@ export async function getProjectById(projectId: Project['id']) {
       success: true,
       message: 'Succesfully retrieved project',
       data: singularProject[0],
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      success: false,
+      message: 'Failed retrieved project',
+      error: JSON.stringify(error),
+    };
+  }
+}
+
+export async function getProjectHeaderData(projectId: Project['id']) {
+  try {
+    const singularProject = await db
+      .select({
+        title: projects.title,
+        description: projects.description,
+        status: projects.status,
+        dueDate: projects.dueDate,
+        createdAt: projects.createdAt,
+      })
+      .from(projects)
+      .where(eq(projects.id, projectId));
+
+    const response = await Promise.all([
+      getMemberCount(projectId),
+      getCompletedTaskCount(projectId),
+      getTotalTaskCount(projectId),
+      getProjectOwner(projectId),
+    ]);
+
+    console.log('project header data', response);
+
+    const memberCount = response[0];
+    const completedTasks = response[1];
+    const totalTasks = response[2];
+    const ownerData = response[3];
+
+    return {
+      success: true,
+      message: 'Succesfully retrieved project',
+      data: {
+        ...singularProject[0],
+        memberCount: memberCount,
+        completedTasks: completedTasks,
+        totalTasks: totalTasks,
+        ownerData: ownerData,
+      },
     };
   } catch (error) {
     console.error(error);
@@ -114,5 +190,48 @@ export async function getProjectDataById(projectId: Project['id']) {
       message: 'Failed retrieved project',
       error: JSON.stringify(error),
     };
+  }
+}
+
+export async function getProjectOwner(projectId: Project['id']) {
+  try {
+    const [ownerData] = await db
+      .select({
+        firstName: users.firstName,
+        lastName: users.lastName,
+        userImgLink: users.imgUrl,
+      })
+      .from(projectMembers)
+      .innerJoin(users, eq(projectMembers.userId, users.id))
+      .where(and(eq(projectMembers.projectId, projectId), eq(projectMembers.role, 'owner')))
+      .limit(1);
+
+    return { ...ownerData };
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function getRecentProjects() {
+  try {
+    const currentUserId = await getCurrentUserId();
+    const maxCount = 4;
+
+    const recentProjects = await db
+      .select({
+        id: projects.id,
+        title: projects.title,
+        createdAt: projects.createdAt,
+        updatedAt: projects.updatedAt,
+      })
+      .from(projects)
+      .innerJoin(projectMembers, eq(projectMembers.projectId, projects.id))
+      .where(eq(projectMembers.userId, currentUserId))
+      .orderBy(projects.updatedAt)
+      .limit(maxCount);
+
+    return recentProjects;
+  } catch (error) {
+    console.error(error);
   }
 }
