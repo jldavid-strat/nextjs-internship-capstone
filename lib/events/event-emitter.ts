0fe786ca38164659950b0aa3e06fb9a5678740a0
jldavid-strat <i_jed.david@stratpoint.com>
2025-09-03 18:01:06 +0800

    modify(event-emitter): made event emitter a single instance in development

--- lib/events/event-emitter.ts
+++ lib/events/event-emitter.ts
@@ -1,14 +1,38 @@
+import { User } from '@/types/db.types';
 import { MoveTaskDataType, ReorderColumnDataType } from '@/types/types';
+
+// [NOTE]
+// this is the core pub/sub primitive shared between the SSE route and server actions
+// requires the node js run time (not edge), therefore switch to redist pub/sub or to pusher client in production
 import { EventEmitter } from 'events';
 
-export const serverEvents = new EventEmitter();
+const MAX_SSE_LISTENERS = 100;
+
+// allow global singleton in dev (Next HMR reloads)
+// In Next JS dev environment, files can be reloaded multile times (aka hot module reloads)
+declare global {
+  var _eventBus: EventEmitter | undefined;
+}
+
+// reuse event bus it it already exist
+// this ensures that the current emitter instance stays alive across reloads
+export const serverEvents = global._eventBus || new EventEmitter();
+
+serverEvents.setMaxListeners(MAX_SSE_LISTENERS);
+
+// In dev, persist across HMR reloads
+if (process.env.NODE_ENV !== 'production') {
+  global._eventBus = serverEvents;
+}
 
 export type TaskMoveEvent = MoveTaskDataType & {
   type: 'task-moved';
+  userId: User['id'];
 };
 
 export type ReorderKanbanColumnEvent = ReorderColumnDataType & {
   type: 'reorder-kanban-columns';
+  userId: User['id'];
 };
 
 export type KanbanServerEvent = TaskMoveEvent | ReorderKanbanColumnEvent;
